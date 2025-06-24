6e714a22ccd9a495e1b75a3cc6e7066072aa3866
Brian-Rotich20 <ralphrotich@gmail.com>
2025-06-24 13:51:30 +0300

    Fix: removed pymysql error causing collectstatic failure

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import pymysql
-pymysql.install_as_MySQLdb()
+
 
 load_dotenv()
 
@@ -149,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/ "staticfiles"
 MEDIA_URL = 'media/'
 STORAGES = {
