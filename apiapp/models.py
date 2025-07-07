7a1c81dc9d90ae78d955c96e2cfc0d771dda8478
Brian-Rotich20 <ralphrotich@gmail.com>
2025-07-07 14:25:32 +0300

    db host name

--- apiapp/models.py
+++ apiapp/models.py
@@ -8,10 +8,11 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
     profile_picture_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
-        return self.email
+        return self.email or self.username
     
 
 class Category(models.Model):
@@ -168,9 +169,3 @@ class CustomerAddress(models.Model):
     def __str__(self):
         return f"{self.customer.email} - {self.street} - {self.city}"
 
-# Phone number field for CustomUser model
-class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-
-    def __str__(self):
-        return self.username  # or email if you prefer
