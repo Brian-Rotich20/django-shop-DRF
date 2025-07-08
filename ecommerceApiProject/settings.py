cab311a93e896a42232a99c3f711eb4a10648b58
Brian-Rotich20 <ralphrotich@gmail.com>
2025-07-08 15:01:48 +0300

    Removed aunthentication

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -149,8 +149,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+   'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # <-- Allow public access by default
     ],
 }
 # Password validation
