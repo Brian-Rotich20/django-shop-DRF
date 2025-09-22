b3ec9cb293f736a9a0e9c523ace6556913cc6d64
Brian-Rotich20 <ralphrotich@gmail.com>
2025-09-22 14:00:55 +0300

    rqt.txt

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "insecure-dev-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]  # For development, this is fine
 ALLOWED_HOSTS = [
