684d3f8554cc5826368b44d99b20159121d71c66
Brian-Rotich20 <ralphrotich@gmail.com>
2025-10-22 14:07:14 +0300

    fix debug to be true

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "insecure-dev-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
